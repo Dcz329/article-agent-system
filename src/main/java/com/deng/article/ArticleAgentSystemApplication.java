8471290ddb20e740840ba8ac32c1f60cab8efb8b
Dcz <2842867137@qq.com>
2026-08-11 19:13:46 +0800

    chore: 补充 mvnw 包装器与 gitignore

---
+++ src/main/java/com/deng/article/ArticleAgentSystemApplication.java
@@ -0,0 +1,13 @@
+package com.deng.article;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArticleAgentSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ArticleAgentSystemApplication.class, args);
+    }
+
+}
